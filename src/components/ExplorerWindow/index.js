b891094cc3de0582f04e81dd1c52a09284326078
padraigfl <padraigflood1@gmail.com>
2019-03-02 02:06:45 +0000

    maximized screen partial fix

--- src/components/ExplorerWindow/index.js
+++ src/components/ExplorerWindow/index.js
@@ -23,49 +23,45 @@ class Explorer extends Component {
   restore = () => this.setState({ maximized: false });
 
   render() {
-    let programWindow = (
-      <WindowExplorer
-        title="Needs default"
-        footer={[
-          { text: "needs 100% width height" },
-          { text: "overflow control" }
-        ]}
-        onClose={() => {
-          /* needs default visibles */
-        }}
-        onMinimize={() => {}}
-        onRestore={this.restore}
-        onMaximize={this.maximize}
-        maximized={this.state.maximized}
-        resizable={!this.state.maximized}
-      />
-    );
-    if (this.state.maximized) {
-      return programWindow;
-    }
     return (
       <Rnd
         size={
           !this.state.maximized
             ? { width: this.state.width, height: this.state.height }
-            : { width: "100%", height: "100%" }
+            : { width: "101%", height: "101%" }
         }
         position={
           !this.state.maximized
             ? { x: this.state.x, y: this.state.y }
-            : { x: 0, y: 0 }
+            : { x: -2, y: -3 }
         }
-        onDragStop={this.updateLocation}
-        onResize={!this.state.maximized && this.resize}
-        // scale={2}
         dragHandleClassName="Window__title"
+        onDragStop={!this.state.maximized && this.updateLocation}
+        disableDragging={this.state.maximized}
+        enableResizing={this.state.maximized ? false : undefined}
+        onResize={!this.state.maximized && this.resize}
         bounds=".w98"
         minWidth={this.props.minWidth}
         minHeight={this.props.minHeight}
         maxWidth={this.props.maxWidth}
         maxHeight={this.props.maxHeight}
+        // scale={2}
       >
-        {programWindow}
+        <WindowExplorer
+          title="Needs default"
+          footer={[
+            { text: "needs 100% width height" },
+            { text: "overflow control" }
+          ]}
+          onClose={() => {
+            /* needs default visibles */
+          }}
+          onMinimize={() => {}}
+          onRestore={this.restore}
+          onMaximize={this.maximize}
+        >
+          Children
+        </WindowExplorer>
       </Rnd>
     );
   }
