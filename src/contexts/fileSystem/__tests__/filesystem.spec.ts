cab55f7bcce428d897691870a6997f82c0b54722
padraigfl <padraigfl@padraigfl.github.io>
2024-03-07 14:20:17 +0000

    fix warning

--- src/contexts/fileSystem/__tests__/filesystem.spec.ts
+++ src/contexts/fileSystem/__tests__/filesystem.spec.ts
@@ -11,7 +11,7 @@ describe('fileSystemReducer', () => {
       logs: {},
     };
   })
-  it(Action.COPY, () => {
+  it(Action.NEW, () => {
     const newSystem = fileSystemReducer(state, {
       type: Action.NEW,
       value: {
@@ -26,4 +26,30 @@ describe('fileSystemReducer', () => {
     });
     expect(getFileFromPath('/mnt/c/usr/me/testFile.txt', newSystem.fileHierarchy)).toBeTruthy();
   });
+  // copies file
+  // fails to copy if duplicate
+  // force override on copy
+  it(Action.COPY, () => {
+    const copySystem = fileSystemReducer(state, {
+      type: Action.COPY,
+      value: {
+        file: '/mnt/c/usr/me/desktop/resume.txt',
+        newLocation: '/mnt/c/usr/me',
+        actionId: 'abcd',
+      },
+    });
+    expect(getFileFromPath('/mnt/c/usr/me/resume.txt', copySystem.fileHierarchy)).toBeTruthy();
+  });
+  // delete
+  // fails to delete if not exist
+  // cant delete protected
+  it(Action.DELETE, () => {});
+  // file exists in new directory
+  // file no longer exists in source directory
+  // file can't be moved for various reasons (same name, protected)
+  it(Action.MOVE, () => {});
+  // old file no longer exists
+  // new file exists
+  // file can't be updated for various reasons (same name, protected)
+  it(Action.UPDATE, () => {});
 })
